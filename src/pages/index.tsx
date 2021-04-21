f0be416d3cc5af454c45d3f3b692f8b30702773b
Alejandro Nanez <alejonanez@gmail.com>
2021-04-20 20:47:59 -0500

    finish example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,48 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
 
-function Home({ countries }) {
+function Home({ countries = [] }) {
   return (
     <div>
       <h1>Countries</h1>
       <pre>{JSON.stringify(countries, null, 2)}</pre>
     </div>
   );
+}
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/",
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+});
+
+async function fetchCountries() {
+  const { data } = await client.query({
+    query: gql`
+      {
+        countries(filter: { continent: { eq: "EU" } }) {
+          name
+          currency
+        }
+      }
+    `,
+  });
+
+  return { countries: data.countries };
+}
+
+export async function getStaticProps() {
+  const { countries } = await fetchCountries();
+  return {
+    props: {
+      countries,
+    },
+  };
 }
 
 export default Home;
